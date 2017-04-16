71a7fd6c9e56425cc5f9b26cf9bd75a350fe4293
J. Tran <j.yn.tran@gmail.com>
2017-04-16 15:55:54 -0400

    Added RGB bars and placeholder for hexcode

--- ColourPicker/src/ColourPicker.js
+++ ColourPicker/src/ColourPicker.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import {
+	AppRegistry,
 	Text,
-	AppRegistry
+	View
 } from 'react-native';
 
 class ColourPicker extends Component {
 	render() {
 		return (
-			<Text>Hello, React Native!</Text>
+			<View style={{flex: 1}}>
+				<View style={{flex: 4, backgroundColor: '#000000'}}>
+					<Text style={{color: '#FFF', fontSize: 64}}>
+						#000000
+					</Text>
+				</View>
+				<View style={{flex: 2, backgroundColor: '#FF0000'}}></View>
+				<View style={{flex: 2, backgroundColor: '#00FF00'}}></View>
+				<View style={{flex: 2, backgroundColor: '#0000FF'}}></View>
+			</View>
 			)
 	}
 }
