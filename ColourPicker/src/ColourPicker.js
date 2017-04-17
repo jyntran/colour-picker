a3a0935317d095701649a2dfaa07cc4b2e48fc02
J. Tran <j.yn.tran@gmail.com>
2017-04-17 14:12:36 -0400

    Modified general design, added placeholder for HSL values

--- ColourPicker/src/ColourPicker.js
+++ ColourPicker/src/ColourPicker.js
@@ -4,19 +4,36 @@ import {
 	Text,
 	View
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 class ColourPicker extends Component {
 	render() {
+		var hue = 0; // degree
+		var saturation = 0; // percent
+		var lightness = 30; // percent
 		return (
 			<View style={{flex: 1}}>
-				<View style={{flex: 4, backgroundColor: '#000000'}}>
-					<Text style={{color: '#FFF', fontSize: 64}}>
-						#000000
+				<View style={{flex: 9,
+					backgroundColor: 'hsl('
+					+ hue + ','
+					+ saturation + '%,'
+					+ lightness + '%)'}}>
+					<Text style={{color: '#FFF', fontSize: 24}}>
+						Hue: {hue} Saturation: {saturation} Lightness: {lightness}
 					</Text>
 				</View>
-				<View style={{flex: 2, backgroundColor: '#FF0000'}}></View>
-				<View style={{flex: 2, backgroundColor: '#00FF00'}}></View>
-				<View style={{flex: 2, backgroundColor: '#0000FF'}}></View>
+				<LinearGradient style={{flex: 1}}
+					start={{x: 0.0, y: 0.0}}
+					end={{x: 1.0, y: 0.0}}
+					colors={[
+						'#F00',
+						'#FF0',
+						'#0F0',
+						'#0FF',
+						'#00F',
+						'#F0F',
+						'#F00']}
+				/>
 			</View>
 			)
 	}
